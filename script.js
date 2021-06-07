cf0527804704b37c39f2e3468eeeda60fe040fc4
saurabh parasar <saurabhparasar3010@gmail.com>
2021-06-07 21:33:34 +0530

    added javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
 
-const modal = document.querySelector("modal");
-const overlay = document.querySelector("overlay");
-const closeModal = document.querySelector("close-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
