5c81aeb041f30057f8c4cd971519ca0a82a8ae21
saurabh parasar <saurabhparasar3010@gmail.com>
2021-06-08 00:33:51 +0530

    added functionality

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener("click", function () {
+    console.log("button clicked");
+    modal.classList.remove("hidden");
+  });
